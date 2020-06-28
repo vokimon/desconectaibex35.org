1fae3232e62402d289bc82380801206e3bb471c9
David García Garzón <voki@canvoki.net>
2020-06-28 03:33:56 +0200

    extracted generators from pelicanconf

---
+++ generators.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+import requests
+from yamlns import namespace as ns
+import html
+
+
+def categories(ctx, *topics):
+    #print(ctx.metadata)
+    ctx.parser.parseChunk(ctx.parent, ''.join([
+        """::: category-list width="64" heigth="auto" style="float:right; margin-left:3ex; text-align: center; background: white"\n""",
+        ] + [
+    """    [![{Topic}]({{static}}/images/categories/{topic}.png "{Topic}")]"""
+    """({{category}}/{Topic})<br>{Topic}\n\n\n""".format(
+            topic = category.lower(),
+            Topic = category.title(),
+        )
+        for category in topics or ctx.metadata.get('category')[0].split(', ')
+    ]))
+
+def rtve(id, title=''):
+    thumb='https://img2.rtve.es/css/rtve.commons/rtve.header.footer/i/logoRTVEes.png'
+    link=f'https://www.rtve.es/v/{id}/'
+    embeded = f"https://secure-embed.rtve.es/drmn/embed/video/{id}/"
+    response=requests.get(f'http://api.rtve.es/api/videos/{id}.json',
+        headers={'Cache-Control': 'no-cache'})
+    if response.ok:
+        metadata = ns.loads(response.text)
+        thumb = metadata.page['items'][0].thumbnail
+        title = html.escape(metadata.page['items'][0].longTitle)
+        link = html.escape(metadata.page['items'][0].htmlUrl)
+    return f"""\
+<div
+    style="width:100%;padding-top:64%;position:relative;border-bottom:1px solid #aaa;display:inline-block;background:#eee;background:rgba(255,255,255,0.9);"
+>
+    <iframe
+        src="{embeded}"
+        name="{title}"
+        style="width:100%;height:90%;position:absolute;left:0;top:0;overflow:hidden;border:none;background-color:transparent;"
+        scrolling="no"
+        allowfullscreen="allowfullscreen"
+    >
+    </iframe>
+    <div
+        style="position:absolute;bottom:0;left:0;font-family:arial,helvetica,sans-serif;font-size:12px;line-height:1.833;display:inline-block;padding:5px 0 5px 10px;"
+    ><span
+            style="float:left;margin-right:10px;"><img
+                style="height:20px;width:auto;background: transparent;padding:0;margin:0;"
+                src="https://img2.rtve.es/css/rtve.commons/rtve.header.footer/i/logoRTVEes.png"
+                alt=""
+            />
+        </span>
+        <a
+            style="color:#333;font-weight:bold;"
+            title="{title}"
+            href="{link}"
+        >
+            <strong>{title}</strong>
+        </a>
+    </div>
+</div>
+"""
+
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,20 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-
-def categories(ctx, *topics):
-	#print(ctx.metadata)
-	ctx.parser.parseChunk(ctx.parent, ''.join([
-		"""::: category-list width="64" heigth="auto" style="float:right; margin-left:3ex; text-align: center; background: white"\n""",
-		] + [
-	"""    [![{Topic}]({{static}}/images/categories/{topic}.png "{Topic}")]"""
-	"""({{category}}/{Topic})<br>{Topic}\n\n\n""".format(
-			topic = category.lower(),
-			Topic = category.title(),
-		)
-		for category in topics or ctx.metadata.get('category')[0].split(', ')
-	]))
-
+import os
+import sys
+sys.path.append(os.curdir)
+import generators
 
 AUTHOR = 'David García Garzón'
 SITENAME = 'Desconecta del IBEX35'
@@ -94,23 +84,24 @@ PLUGINS=[
 ]
 
 MARKDOWN = {
-	'extension_configs': {
-		'markdown.extensions.codehilite': {
-			'css_class': 'highlight',
-		},
+    'extension_configs': {
+        'markdown.extensions.codehilite': {
+            'css_class': 'highlight',
+        },
         'markdown.extensions.admonition': {},
         'markdown.extensions.toc': {},
         'markdown.extensions.extra': {},
         'markdown.extensions.tables': {},
-		'markdown.extensions.attr_list': {},
-		'customblocks': {
-			'generators': {
-				'categories': categories,
-			},
-			'config': {
-				'youtube_inlineFluidStyles': True,
-			},
-		}
+        'markdown.extensions.attr_list': {},
+        'customblocks': {
+            'generators': {
+                'categories': generators.categories,
+                'rtve': generators.rtve,
+            },
+            'config': {
+                'youtube_inlineFluidStyle': True,
+            },
+        }
     },
     'output_format': 'html5',
 }
@@ -120,7 +111,7 @@ THEME='elegant'
 # ELEGANT THEME CUSTUMIZATION
 THEME_STATIC_DIR = 'theme/'
 THEME_TEMPLATES_OVERRIDES = [
-	'templates',
+    'templates',
 ]
 SEARCH_BOX = False
 LANDING_PAGE_TITLE = "Desconecta del IBEX-35"
@@ -144,3 +135,5 @@ SITE_UPDATED = datetime.date.today()
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# vim: et ts=4 sw=4
