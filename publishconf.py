5b72069791ae9314dacf63a2595c49ed760e96d4
David García Garzón <voki@canvoki.net>
2020-10-30 08:54:08 +0100

    solving relative urls

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'http://desconexionibex35.org'
+SITEURL = 'https://desconexionibex35.org'
+SITEURL = 'https://vokimon.github.io/desconectaibex35.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
